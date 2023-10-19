70a89f7a22e650802e25ef7ff0925507eb5370c9
Azhar Ibn Mostafiz <theazharul@gmail.com>
2023-10-19 07:33:51 +0600

    some changes

--- build-propovoice.py
+++ build-propovoice.py
@@ -1,1 +1,20 @@
- 
+#!/usr/bin/env python
+
+import typer
+import subprocess
+# from PyInquirer import prompt, print_json, Separator
+# from prompt_toolkit import prompt
+from rich import print as rprint
+
+app = typer.Typer()
+
+@app.command("hi")
+def sample_func():
+    rprint("[red bold]Hi[/red bold] [yellow]World[yello]")
+
+@app.command("hello")
+def sample_func():
+    rprint("[red bold]Hello[/red bold] [yellow]World[yello]")
+
+if __name__ == "__main__":
+    app()
